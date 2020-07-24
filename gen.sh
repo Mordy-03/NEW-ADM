2482c1966536c169909bd5bde617dcfa7c61874d
Mordy-03 <67871399+Mordy-03@users.noreply.github.com>
2020-07-23 19:09:42 -0500

    Add files via upload

---
+++ gen.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+IVAR="/etc/http-instas"
+SCPT_DIR="/etc/SCRIPT"
+SCPresq="aHR0cHM6Ly9naXRsYWIuY29tL0FNWDIwL2dlbi8tL3Jhdy9tYXN0ZXIvcmVxdWVzdA=="
+SUB_DOM='base64 -d'
+veryfy_fun () {
+[[ ! -d ${IVAR} ]] && touch ${IVAR}
+[[ ! -d ${SCPT_DIR} ]] && mkdir ${SCPT_DIR}
+unset ARQ
+case $1 in
+"gerar.sh")ARQ="/usr/bin/";;
+"http-server.py")ARQ="/bin/";;
+*)ARQ="${SCPT_DIR}/";;
+esac
+mv -f $HOME/$1 ${ARQ}/$1
+chmod +x ${ARQ}/$1
+}
+function_verify () {
+  permited=$(curl -sSL "https://gitlab.com/AMX20/amx/-/raw/master/Control-IP")
+  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+  echo -e "\n\033[1;33m--------------------------------------------------------------------\n
+🚫 𝗔𝗧𝗘𝗡𝗖𝗜𝗢𝗡 𝗟𝗔 𝗜𝗣 $(wget -qO- ipv4.icanhazip.com)  𝗡𝗢 𝗘𝗦𝗧𝗔 𝗔𝗨𝗧𝗢𝗥𝗜𝗭𝗔𝗗𝗔   ❎      \n
+♨️ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧𝗘 𝗔 @𝗠𝗼𝗿𝗱𝗿𝗲𝘁𝗢𝗳𝗶𝗰𝗶𝗮𝗹    \n
+--------------------------------------------------------------------\n"
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+  [[ -d /etc/SCRIPT ]] && rm -rf /etc/SCRIPT
+  exit 1
+  } || {
+  ### INTALAR VERCION DE SCRIPT
+  v1=$(curl -sSL "https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Vercion")
+  echo "$v1" > /etc/versin_script
+  }
+}
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+meu_ip () {
+MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MIP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+echo "$IP" > /usr/bin/vendor_code
+}
+meu_ip
+clear
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m𝗗𝗘𝗦𝗖𝗔𝗥𝗚𝗔𝗡𝗗𝗢 𝗔𝗥𝗖𝗛𝗜𝗩𝗢𝗦...    "
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+cd $HOME
+REQUEST=$(echo $SCPresq|$SUB_DOM)
+wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
+function_verify
+[[ -e $HOME/lista-arq ]] && {
+for arqx in `cat $HOME/lista-arq`; do
+echo -ne "\033[1;33mDescargando archivo: \033[1;31m[$arqx] "
+wget -O $HOME/$arqx ${REQUEST}/${arqx} > /dev/null 2>&1 && {
+echo -e "\033[1;31m- \033[1;32mRecibido con exito!"
+[[ -e $HOME/$arqx ]] && veryfy_fun $arqx
+} || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
+done
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/theonejpas/configuracion-raiz-nueva/master/Install/trans &> /dev/null
+[[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
+[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] || apt-get install lolcat -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || apt-get install apache2 -y &>/dev/null
+sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
+service apache2 restart > /dev/null 2>&1 &
+IVAR2="/etc/key-gerador"
+echo "$Key" > $IVAR
+cp $HOME/lista-arq /etc/SCRIPT
+cp /bin/http-server.sh /etc/SCRIPT
+mv /etc/SCRIPT/http-server.sh /etc/SCRIPT/http-server.py
+wget https://raw.githubusercontent.com/theonejpas/configuracion-raiz-nueva/master/gerador/gerar.sh &>/dev/null
+mv gerar.sh /etc/SCRIPT
+cd /etc/SCRIPT
+rm -rf FERRAMENTA KEY KEY! INVALIDA!
+rm $HOME/lista-arq
+sed -i -e 's/\r$//' /usr/bin/gerar.sh
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo "/usr/bin/gerar.sh" > /usr/bin/gerar && chmod +x /usr/bin/gerar
+echo -e "\033[1;33m Utiliza el comando \033[1;31mgerar.sh o gerar \033[1;33mpara administrar sus keys y
+ actualizar la base del servidor"
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+} || {
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33mKey Invalida!"
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+}
+echo -ne "\033[0m"
+apt-get install netcat -y &>/dev/null
+apt-get install net-tools -y &>/dev/null
+sed -i -e 's/\r$//' /usr/bin/gerar.sh
